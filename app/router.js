76c00eddd76f1af40ef387c9f48ca9642d75c0fc
MUHM <m@ie.gl>
2021-04-09 15:50:31 +0800

    增加热点抓取

--- app/router.js
+++ app/router.js
@@ -10,11 +10,14 @@ module.exports = async app => {
   // socket.io
   // io.of('/').route('exchange', io.controller.nsp.exchange);
 
-  router.get('/api/test', controller.proxy.index);
+  // router.get('/api/test', controller.proxy.index);
   // 图形验证码
   router.get('/api/imgcode/:random', controller.imgCode.index);
   // 外部接口-天气
   router.get('/api/weather', controller.external.weather.index);
+  for (const item in controller.external.crawler) {
+    router.get(`/api/crawler/${item.toLowerCase()}`, controller.external.crawler[item]);
+  }
   // 基本信息
   router.get('/api/admin/systemconst/webconfig', controller.admin.systemConst.webConfig);
 
